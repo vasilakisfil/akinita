877124233caa0b11b14e31ea85fce8155fe8866e
Filippos Vasilakis <vasilakisfil@gmail.com>
2010-07-19 11:48:38 +0000

    Προστεθηκαν οι insert εντολές στο αρχείο insert_db.sql.
    git-svn-id: http://127.0.0.1/svn/earth@25 7c98068c-6452-4d1e-908a-a0b02d9f98b2

--- akinita_db.sql
+++ akinita_db.sql
@@ -1,74 +1,75 @@
 ﻿
+drop DATABASE if exists akinita;
+create DATABASE akinita;
 
+use akinita;
 
-drop DATABASE if exists `akinita`;
-create DATABASE `akinita`;
-
-use `akinita`;
-
-create table `users`
+create table users
 (
-	`username` char(20) not null,
-	`password` char(16) not null,
-	`email` char(20) not null,
-	`name` char(16),
-	`surname` char(16),
-	`user_type` enum('Adminstrator','user'),
-	unique(`email`),
-	primary key(`username`)
+	username char(20) not null,
+	password char(16) not null,
+	email char(50) not null,
+	name char(16),
+	surname char(16),
+	user_type enum('A','U'),
+	unique(email),
+	primary key(username)
 )engine=innoDB default character set utf8 collate utf8_general_ci;
 
-create table `telephone`
+create table telephone
 (
-	`tel_id` int(10) auto_increment,
-	`home` char(10),
-	`mobile1` char(10) not null,
-	`mobile2` char(10),
-	`other` char(10),
-	`user_id` char(20),
-	primary key (`tel_id`),
-	foreign key(`user_id`) references `users` (`username`)
+	tel_id int auto_increment,
+	home char(10),
+	mobile1 char(10) not null,
+	mobile2 char(10),
+	other char(10),
+	user_id char(20) not null,
+	primary key(tel_id),
+	constraint phones_constr,
+	foreign key(user_id) references users(username)
+	on delete cascade on update cascade
 )engine=innoDB default character set utf8 collate utf8_general_ci auto_increment=1;
 
-create table `property`
+create table property
 (
-	`prop_id` int not null auto_increment primary key,
-	`address` char(100) not null,
-	`price` int(15) not null,
-	`offer_type` ENUM('P','L'),   -- <-----
-	`area` int(10) not null,
-	`constr_date` date,
+	prop_id int not null auto_increment,
+	address char(100) not null,
+	price int(15) not null,
+	offer_type ENUM('S','L'),   -- <-----
+	area int(10) not null,
+	constr_date date,
 	/*coordinates */
-	`photos` mediumblob,
-	`comments` text,
-	`user_id` varchar(20),
-	foreign key(`user_id`) references `users` (`username`)
+	photos mediumblob,
+	comments text,
+	user_id char(20),
+	primary key(prop_id),
+	foreign key(user_id) references users(username)
 )engine=innoDB default character set utf8 collate utf8_general_ci auto_increment=1;
 
-create table `facilities`
+create table facilities
 (
-	`fac_id` int not null auto_increment,
-	`facility` varchar(20) not null,
-	primary key(`fac_id`)
+	fac_id int not null auto_increment,
+	facility char(20) not null,
+	primary key(fac_id)
 )engine=innoDB default character set utf8 collate utf8_general_ci auto_increment=1;
 
-create table `fac_prop`
+create table fac_prop
 (
-	`fac_id` int,
-	`prop_id` int,
-	primary key(`fac_id`,`prop_id`)	
+	prop_id int,
+	fac_id int,
+	primary key(fac_id,prop_id)	
 )engine=innoDB default character set utf8 collate utf8_general_ci;
 
-create table `categories`
+create table categories
 (
-	`cat_id` int not null auto_increment,
-	`category` char(20) not null,
-	primary key(`cat_id`)
+	cat_id int not null auto_increment,
+	category char(20) not null,
+	primary key(cat_id)
 )engine=innoDB default character set utf8 collate utf8_general_ci auto_increment=1;
 
-create table `cat_prop`
+create table cat_prop
 (
-	`cat_id` int,
-	`prop_id` int,
-	primary key(`cat_id`,`prop_id`)
+	prop_id int,
+	cat_id int,
+	primary key(cat_id,prop_id)
 );
---
+++ insert_db.sql
@@ -0,0 +1,58 @@
+﻿
+use akinita;
+
+insert into users (username,password,email,user_type) values
+('vasilakis','19881988','vasilakis@gmail.com','A'),
+('mits','1234','dklisiaris@hotmail.com','A'),
+('konstantina','1234','konstkar@hotmail.com','A'),
+('vassis','ntavas','esaamitisforever@hotmail.com','U'),
+('guest','guestpass','guest@guest.gr','U'),
+('alex','11072007','alenumber11@hotmail.com','U');
+
+insert into telephone (mobile1,user_id) values
+('6932735244','vasilakis'),
+('3983742883','guest'),
+('3928423987','vassis'),
+('9823428838','mits'),
+('9823479283','konstantina'),
+('6666666666','alex');
+
+
+insert into property (prop_id,address,price,offer_type,area,user_id) values
+(1,'matzarou 15',45000,'S',35,'konstantina'),
+(2,'aliartou 19',65000,'S',59,'vasilakis'),
+(3,'kwsth palama 5',75000,'S',78,'alex'),
+(4,'kefallhnias 20-24',70000,'S',72,'guest'),
+(5,'satwmvriandou 35-37',300,'L',22,'mits'),
+(6,'ageirou 8',480,'L',75,'vassis');
+
+
+insert into facilities (fac_id,facility) values
+(1,'proedriko mpanio'),
+(2,'mpaniera'),
+(3,'ntouz'),
+(4,'epiplwmeno');
+
+insert into fac_prop (prop_id,fac_id) values
+(2,1),
+(1,2),
+(3,2),
+(6,2),
+(4,2),
+(5,4),
+(5,3);
+
+insert into categories (cat_id,category) values
+(1,'studio'),
+(2,'gkarsoniera'),
+(3,'duari'),
+(4,'triari');
+
+insert into cat_prop (prop_id,cat_id) values
+(5,1),
+(1,2),
+(2,3),
+(4,4),
+(5,4),
+(6,4);
+
