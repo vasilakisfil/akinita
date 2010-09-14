544fa6807c084f8d7c5dede4e76b80d6804c53f7
Filippos Vasilakis <vasilakisfil@gmail.com>
2010-09-14 15:36:42 +0000

    Βελτιώθηκε και προστέθηκαν σχόλια στο editProperty.php..
    git-svn-id: http://127.0.0.1/svn/earth@138 7c98068c-6452-4d1e-908a-a0b02d9f98b2

--- site/editProperty.php
+++ site/editProperty.php
@@ -1,24 +1,14 @@
 ﻿<?php
 /****************************************************************************************
-*	
+*		Auto to arxeio parexei ta forms gia thn epe3ergasia enos akinhtou
 *****************************************************************************************/
 //including required files
 include('includes.php');
 
 
-/*// dhmiourgoume topikes metavlhtes gia ka8e SESSION metavlhth.An h SESSION metavlhth
+// dhmiourgoume topikes metavlhtes gia ka8e SESSION metavlhth.An h SESSION metavlhth
 // den exei te8ei sthn topikh metavlhth eisagoume thn timh NULL
-if(isset($_POST['oldPassword'])) $oldPas=$_POST['oldPassword']; else $oldPas=NULL;
-if(isset($_POST['newPassword1'])) $pas1=$_POST['newPassword1']; else $pas1=NULL;
-if(isset($_POST['newPassword2'])) $pas2=$_POST['newPassword2']; else $pas2=NULL;
-if(isset($_POST['newEmail'])) $newMail=$_POST['newEmail']; else $newMail=NULL;
-if(isset($_POST['newMob1'])) $newMob1=$_POST['newMob1']; else $newMob1=NULL;
-if(isset($_POST['newName'])) $newName=$_POST['newName']; else $newName=NULL;
-if(isset($_POST['newLast'])) $newLast=$_POST['newLast']; else $newLast=NULL;
-if(isset($_POST['newMob2'])) $newMob2=$_POST['newMob2']; else $newMob2=NULL;
-if(isset($_POST['newHome'])) $newHome=$_POST['newHome']; else $newHome=NULL;
-if(isset($_POST['newOthr'])) $newOthr=$_POST['newOthr']; else $newOthr=NULL;
-if(isset($_POST['privilege'])) $priv=$_POST['privilege']; else $priv=NULL;*/
+
 if(isset($_POST['newAddress'])) $newAddress=$_POST['newAddress']; else $newAddress=NULL;
 if(isset($_POST['newType'])) $newType=$_POST['newType']; else $newType=NULL;
 if(isset($_POST['newArea'])) $newArea=$_POST['newArea']; else $newArea=NULL;
@@ -31,19 +21,20 @@ try{
 	//elegxoume an o xrhsths einai swsta sundedemenos
 	check_valid_user();
 
-	//ean tipota den exei te8ei(dld einai h prwth fora pou anoigei h selida
+	//ean tipota den exei te8ei(dld einai h prwth fora pou anoigei h selida)
 	if(!$newAddress&&!$newArea&&!$newType&&!$newConstrDate&&!$newPrice)
 	{
 		//apla emfanize to header..
 		dispHeader("Eπεξεργασία αγγελίας $propId");
-		//tην αγγελία..
+		//την αγγελία..
 		showProperty($propId);
 		echo "<br /><br />";
-		//..kai tis epiloges gia thn allagh tou profil
+		//..kai tis epiloges gia thn allagh ths aggelias
 		dispPropOptions($propId);
 		dispFooter();
 
 	}
+	//alliws elegxoume ena ena ta forms kai gia ka8e form kanoume ena update sthn vash me thn nea timh...
 	else
 	{
 		if($newAddress)
@@ -67,154 +58,7 @@ try{
 			$message="Η τιμή του ακινήτο άλλαξε!";
 		}
 	}
-	/*else
-	{
-		//ean o tupos xrhsth einai kanonikos User(dld oxi Admin)
-		if($type=="User")
-		{
-			//ean exoun te8ei kai ta 3 passwords..
-			if($oldPas && $pas1 && $pas2)
-			{
-				//eleg3e an ta 2 kainourgia passwords einai idia meta3u tous
-				if($pas1 != $pas2)
-				{
-					throw new Exception('Οι κωδικοί που δώσατε είναι διαφορετικοί!');
-				}
-				//an einai
-				else
-				{
-					//check_pass($user,$oldPas); <-------------- ?
-					//eleg3e an to palio password pou edwse einai to swsto
-					$new=db_check("users","username","password","'$user'","'$oldPas'");
-					if($new==false) throw new Exception('Wrong password');
-					//an einai kane update to password tou xrhsth
-					db_update("users","username","password","'$user'","'$pas1'");
-					$message="Έγινε η αλλαγή του κωδικού σας!";
-				}
-			}
-			//an kapoio apo ta 3 den exei kataxwrh8ei peta exception
-			else if(($oldPas && (!$pas1 || !$pas2)) || ($pas1 && (!$oldPas || !$pas2)) || ($pas2 && (!$oldPas || !$pas1)))
-			{
-				throw new Exception('Δεν έχετε συμπληρώσει σωστά την φόρμα.Παρακαλούμε προσπαθείστε ξανά.');
-			}
-		}
-		//an o xrhsths einai admin
-		else
-		{
-			//den xreiazetai na 3erei to palio password tou xrhsth kai etsi exei mono 2 inputs boxes gia password
-			if($pas1 && $pas2)
-			{
-				//an ta 2 kainourgia passwords pou edwse den einai swsta peta exception
-				if($pas1 != $pas2)
-				{
-					throw new Exception('Οι δύο καινούργιοι κωδικοί είναι διαφορετικοί');
-				}
-				//alliws an einai
-				else
-				{
-					//check_pass($user,$oldPas); <--------------- ?
-					//ananewse to password tou xrhsth
-					db_update("users","username","password","'$user'","'$pas1'");
-					$message="Ο κωδικός του χρήστη $user άλλαξε!";
-				}
-			}
-			//an kapoio apo ta 2 den exei kataxwrh8ei swsta peta exception
-			else if((!$pas1 && $pas2) || ($pas1 && !$pas2))
-			{
-				throw new Exception('Δεν έχετε συμπληρώσει σωστά την φόρμα.Παρακαλούμε προσπαθείστε ξανά.');
-			}
-		}
-		//an to pedio gia to email einai kataxwrhmeno
-		if($newMail)
-		{
-			//eleg3e an to email einai valid kai an den einai peta exception
-			$ret=valid_email($newMail);
-			if($ret==false) throw new Exception('Το email που δώσατε δεν είναι έγκυρο.Παρακαλούμε προσπαθείστε ξανά.');
-			//an einai ananewse to email tou xrhsth
-			db_update("users","username","email","'$user'","'$newMail'");
-			if($val_user!=$user) $message="To email του χρηστη $user άλλαξε!";
-			else $message="Το email σας αλλαξε!";
-		}
-		//eleg3e an to pedio gia to kinito1 einai kataxwrhmeo
-		if($newMob1)
-		{
-			//SOS EDW PREPEI NA MPEI ELEGXOS GIA 10 ARI8MOUS
-			//ananewse to kinito1 tou xrhsth
-			db_update("telephone","user_id","mobile1","'$user'",$newMob1);
-			if($val_user!=$user) $message="To κινητό1 του χρηστη $user άλλαξε!";
-			else $message="Το κινητό σας αλλαξε!";
-		}
-		//elg3e an to pedio Onoma einai kataxwrhmeno
-		if($newName)
-		{
-			//an einai ananewse to Onoma tou xrhsth
-			db_update("users","username","name","'$user'","'$newName'");
-			if($val_user!=$user) $message="To όνομα του χρηστη $user άλλαξε!";
-			else $message="Το όνομά σας αλλαξε!";
-		}
-		//eleg3e an to pedio Epi8eto einai kataxwrhmeno
-		if($newLast)
-		{
-			//an einai ananewse to epi8eto tou xrhsth
-			db_update("users","username","surname","'$user'","'$newLast'");
-			if($val_user!=$user) $message="To επίθετο του χρηστη $user άλλαξε!";
-			else $message="Το επίθετό σας αλλαξε!";
-		}
-		//eleg3e an to pedio kinito2 einai kataxwrhmeno
-		if($newMob2)
-		{
-			//SOS EDW PREPEI NA MPEI ELEGXOS GIA 10 ARI8MOUS
-			//an einai ananewse to kinito2 tou xrhsth
-			db_update("telephone","user_id","mobile2","'$user'",$newMob2);
-			if($val_user!=$user) $message="To κινητό2 του χρηστη $user άλλαξε!";
-			else $message="Το κινητό σας αλλαξε!";
-		}
-		//eleg3e an to pedio gia to sta8ero thlefwno einai kataxwrhmeno
-		if($newHome)
-		{
-			//SOS EDW PREPEI NA MPEI ELEGXOS GIA 10 ARI8MOUS
-			//an einai ananewse to sta8ero thlefwno tou xrhsth
-			db_update("telephone","user_id","home","'$user'",$newHome);
-			if($val_user!=$user) $message="To σταθερό τηλέφωνο του χρηστη $user άλλαξε!";
-			else $message="Το σταθερό σας αλλαξε!";
-		}
-		//eleg3e an to pedio gia to allo thlefwno einai kataxwrhmeno
-		if($newOthr)
-		{
-			//SOS EDW PREPEI NA MPEI ELEGXOS GIA 10 ARI8MOUS
-			//an einai ananewse to allo thlefwno tou xrhsth
-			db_update("telephone","user_id","other","'$user'",$newOthr);
-			if($val_user!=$user) $message="To άλλο τηλέφωνο του χρηστη $user άλλαξε!";
-			else $message="Το άλλο τηλέφωνό σας αλλαξε!";
-		}
-		//an to privilege exei parei thn timh "Admin" (mono gia Admin users)
-		if($priv=="Admin")
-		{
-			//tote kane to xrhsth Admin
-			db_update("users","username","user_type","'$user'","A");
-			$message="Ο χρήστης $user μόλις έγινε διαχειριστής!";
-		}
-		//alliws
-		else if($priv=="User")
-		{
-			//kane ton xrhsth User
-			db_update("users","username","user_type","'$user'","U");
-			$message="Ο χρήστης $user μόλις έγινε ταπεινός User!";
-		}
-		
-		if($type=="Admin") dispHeader("Το profil του χρήστη $user");
-		else dispHeader("Το profil σας");
-		//h sunarthsh showUserProfile($user) emfanizei to profil tou xrhsth me username $user
-		showUserProfile($user);
-		echo "<br />";
-		echo $message;
-		echo "<br /><br />";
-		//h sunarthsh displayUserOptions($user,$type) emfanizei tis epiloges gia ton xrhsth me username $user
-		//an to $type einai Admin tote emfanizontai parapanw epiloges
-		displayUserOptions($user,$type);
-		dispFooter();
-		$message='';*/
-//////////////////////////////////////////////////////////////////////////////////////////////
+		//sto telos emfanizoume pali thn aggelia me tis allages kai ena munhma ti alla3e...
 		dispHeader("Eπεξεργασία αγγελίας $propId");
 		//tην αγγελία..
 		showProperty($propId);
